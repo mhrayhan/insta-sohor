8b079c8250b624ce37ea36e17ce4fc914e403ae6
mhrayhan <mhrayhan430@gmial.com>
2022-03-11 00:48:14 +0600

    accordion add in footer

--- js/app.js
+++ js/app.js
@@ -36,7 +36,7 @@ const switchTab = (id) => {
         document.getElementById( "liked" ).style.display = "none";
         document.getElementById( "reported" ).style.display = "none";
     } else if (id === "liked") {
-        document.getElementById( "liked" ).style.display = "block";
+        document.getElementById( "liked" ).style.display = "grid";
         document.getElementById( "posts" ).style.display = "none";
         document.getElementById( "reported" ).style.display = "none";
 
