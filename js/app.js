029dbc64f8c2c912da819a3e82c4f1d796654285
mhrayhan <mhrayhan430@gmial.com>
2022-03-11 00:59:19 +0600

    bonus part 1 qs done

--- js/app.js
+++ js/app.js
@@ -36,13 +36,13 @@ const switchTab = (id) => {
         document.getElementById( "liked" ).style.display = "none";
         document.getElementById( "reported" ).style.display = "none";
     } else if (id === "liked") {
-        document.getElementById( "liked" ).style.display = "grid";
+        document.getElementById( "liked" ).style.display = "block";
         document.getElementById( "posts" ).style.display = "none";
         document.getElementById( "reported" ).style.display = "none";
 
         displayLikedPosts();
     } else {
-        document.getElementById( "reported" ).style.display = "grid";
+        document.getElementById( "reported" ).style.display = "block";
         document.getElementById( "posts" ).style.display = "none";
         document.getElementById( "liked" ).style.display = "none";
 
